97977a6dea8109cf97bf97a941fa8196236959c3
icoder-Ahish <92961202+icoder-Ahish@users.noreply.github.com>
2023-04-20 14:41:19 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,54 +1,9 @@
-names = []
-phone_numbers = []
-label = []
+import streamlit as st
 
-# Take a user Input  
-num = int(input("How many contact  do you want to add:"))
+st.header('streamlit selectbox demo')
 
-for i in range(num):
-    name1 = input("Name: ")
-    phone_number1 =  int(input("Phone Number: "))
-    print("Give Some Label to this Number") 
-    label1 = input("Label: ") 
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
 
-    label.append(label1)
-    names.append(name1)
-    phone_numbers.append(phone_number1)
-    
-   
-
-print("Do You Wish To Print Your Contact list:- ")
-
-var1 = input("Type 'Yes' or 'No'")
-
-if var1 == 'yes':
-    print("\nName\t\tPhone Number\t\tLabel")
-
-    for i in range(num):
-        print(names[i], "\t\t", phone_numbers[i], "\t\t",label[i])
-
-       
-else:
-    print("I think You don't Want To Print Your Contact List\n")
-
-
-print("Do You Want To search Your Contact \n")
-
-var2 = input("Type 'Yes' or 'No'")
-
-if var2 == 'yes':
-
-    search_term = input("\nEnter search term: ")
-    print("Search result:")
-
-    if search_term in names:
-            index = names.index(search_term)
-            phone_number = phone_numbers[index]
-            print("Name:",search_term , "Phone Number:",phone_number)
-
-
-    else:
-            print("Name Not Found")
-
-else:
-    print("Thanku.......")        
+st.write('Your favorite color is ', option)
