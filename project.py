dd080bb4f1ff911811ea342ec9d85228ec4236ac
icoder-Ahish <92961202+icoder-Ahish@users.noreply.github.com>
2025-07-13 20:59:42 +0530

    Delete project.py

--- project.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.header('streamlit selectbox demo')
-
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
-
-st.write('Your favorite color is ', option)
